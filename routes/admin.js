e6be886ed4539d03365323b55840c7aac5e5ffca
Hans <hans00@cs.washingtion.edu>
2020-06-27 15:47:36 -0700

    adding comments and changing configs

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,36 @@
-const path = require('path');
+/**
+ * This class handles all of the admin-related requests
+ */
 
-const express = require('express');
-
-const rootDir = require('../util/path');
-
+// Imports
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 const router = express.Router();
 
+// An array of products to be stored
+// Right now, it is just a variable, will use files or databases
+// To store them later
 const products = [];
 
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+// getting the add project page with the form
+// use the HTML template to dynamically render HTML pages
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true
   });
 });
 
-// /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+// Keeping track of the new product information and redict
+router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect('/');
+  res.redirect("/");
 });
 
+// Exports
 exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,28 @@
-const path = require('path');
+/**
+ * This class handles all of the shop-related requests
+ */
 
-const express = require('express');
-
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
+// Imports
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+// getting the shop main page
+// use the HTML template to dynamically render HTML pages
+// and add products
+router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {
+  res.render("shop", {
     prods: products,
-    pageTitle: 'Shop',
-    path: '/',
+    pageTitle: "Shop",
+    path: "/",
     hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+    // activeShop: true,
+    // productCSS: true
   });
 });
 
+// Exports
 module.exports = router;
