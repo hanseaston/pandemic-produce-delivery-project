ac432b347e26b14af92153474be80a5ab0e6fcbb
Hans <hans00@cs.washington.edu>
2020-07-03 14:35:47 -0700

    committing login and logout logic, hides add to cart functionality when logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,3 +10,10 @@ exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
   res.redirect("/");
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
