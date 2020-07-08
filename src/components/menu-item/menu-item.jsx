a3e8142f8fe5b0b94b2bb3bc5791f7d5ed83c710
Hans <hans00@cs.washington.edu>
2020-07-07 22:38:44 -0700

    setting up react project and adding hopmepage/directory components

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./menu-tem.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Directory from "../components/directory/directory";
+import "./homepage.scss";
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
