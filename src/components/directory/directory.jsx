54270d8ae13cc95560f8867b99b0dcc201ae7c15
Hans <hans00@cs.washington.edu>
2020-07-11 20:17:40 -0700

    refactoring directory class into functional component using redux reducer and selector

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,58 +1,20 @@
-import React, { Component } from "react";
-
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import MenuItem from "../menu-item/menu-item";
+import { selectDirectorySections } from "../../redux/frontpage-directory/directorySelector";
 import "./directory.scss";
 
-class Directory extends Component {
-  constructor() {
-    super();
-    this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
-    };
-  }
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import userReducer from "./users/userReducer";
 import cartReducer from "./cart/cartReducer";
 import shopReducer from "./shop/shopReducer";
+import directoryReducer from "./frontpage-directory/directoryReducer";
 
 export default combineReducers({
   user: userReducer,
   cart: cartReducer,
   shop: shopReducer,
+  directory: directoryReducer,
 });
