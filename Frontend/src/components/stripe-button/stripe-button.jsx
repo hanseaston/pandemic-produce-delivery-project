19ce60a7ad238b1f7c6a359a32df81fa24608917
Hans Zhang <hans00@cs.washington.edu>
2020-07-23 17:37:01 -0700

    working on the payment routing handler

--- Frontend/src/components/stripe-button/stripe-button.jsx
+++ Frontend/src/components/stripe-button/stripe-button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,20 +8,37 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51H4B83HbjepOj68glYv8EjGSuqAhIEsb7R69JaNDNSPF0L81DQupI60zljBNK5MPomdu40dDeAeJtMCUuwNFYY8e00nvkvY6z3";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        //TODO: might want to show a more helpful success message
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        //TODO: similar here
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label='Pay'
+      name='Seattle Produce Delivery'
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel='Pay'
       token={onToken}
       stripeKey={publishableKey}
     />
--- Frontend/src/pages/cart-checkout/cart-checkout.jsx
+++ Frontend/src/pages/cart-checkout/cart-checkout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import StripeCheckout from "../../components/stripe-button/stripe-button";
 import CheckoutItem from "../../components/checkout-item/checkout-item";
-import CustomButton from "../../components/custom-button/custom-button";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import {
@@ -10,36 +9,43 @@ import {
 } from "../../redux/cart/cartSelector";
 import "./cart-checkout.scss";
 
-const CheckoutPage = ({ cartItems, total }) => {
-  console.log(cartItems);
-  return (
-    <div className='checkout-page'>
-      <div className='checkout-header'>
-        <div className='header-block'>
-          <span>Product</span>
+class CheckoutPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { cartItems, total } = this.props;
+    return (
+      <div className='checkout-page'>
+        <div className='checkout-header'>
+          <div className='header-block'>
+            <span>Product</span>
+          </div>
+          <div className='header-block'>
+            <span>Description</span>
+          </div>
+          <div className='header-block'>
+            <span>Quantity</span>
+          </div>
+          <div className='header-block'>
+            <span>Price</span>
+          </div>
+          <div className='header-block'>
+            <span>Remove</span>
+          </div>
         </div>
-        <div className='header-block'>
-          <span>Description</span>
-        </div>
-        <div className='header-block'>
-          <span>Quantity</span>
-        </div>
-        <div className='header-block'>
-          <span>Price</span>
-        </div>
-        <div className='header-block'>
-          <span>Remove</span>
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+        <div className='total'>TOTAL: ${total.toFixed(2)}</div>
+        <div>
+          <StripeCheckout price={total} />
         </div>
       </div>
-      {cartItems.map((cartItem) => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-      <div className='total'>TOTAL: ${total.toFixed(2)}</div>
-      <CustomButton inverted>Pay</CustomButton>
-      {/* <StripeCheckout price={total}></StripeCheckout> */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
