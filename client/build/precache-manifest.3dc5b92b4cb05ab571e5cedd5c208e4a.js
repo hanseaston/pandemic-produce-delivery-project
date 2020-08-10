a11c3ee550613cf5418915dfac074ff0f51bfd17
Hans Zhang <hans00@cs.washington.edu>
2020-08-09 17:59:57 -0700

    adding more dependencies

---
+++ client/build/precache-manifest.3dc5b92b4cb05ab571e5cedd5c208e4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e0cb1b711a8cd1e50d74c1f441b0ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ba0ab28807cb05d8dce",
+    "url": "/static/css/main.8fda0ef3.chunk.css"
+  },
+  {
+    "revision": "b1fffd333bcb9cdc5adc",
+    "url": "/static/js/2.8c5e4a46.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/static/js/2.8c5e4a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba0ab28807cb05d8dce",
+    "url": "/static/js/main.b727b8fa.chunk.js"
+  },
+  {
+    "revision": "dbd1f0cd91b4a04cb746",
+    "url": "/static/js/runtime-main.894d8643.js"
+  },
+  {
+    "revision": "88852d75d1e4884ab1ca004ff233de68",
+    "url": "/static/media/potato.88852d75.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
