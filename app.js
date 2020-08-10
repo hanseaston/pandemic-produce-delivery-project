9cba4c79d9c5cc6eac7253e56e1bb1a3fe468d46
Hans Zhang <hans00@cs.washington.edu>
2020-08-09 22:27:56 -0700

    chaninging it back..

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const checkoutRouter = require("./routes/checkout");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== "production") {
