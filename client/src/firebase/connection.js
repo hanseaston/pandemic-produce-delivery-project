6e3c611f0e1fe5c789fe315df03e9e65ecae470a
Hans Zhang <hans00@cs.washington.edu>
2020-08-14 20:22:08 -0700

    adding back public keys

---
+++ client/src/firebase/connection.js
@@ -0,0 +1,13 @@
+// Replace the content here with your own firebase config
+const config = {
+  apiKey: "AIzaSyCfqM9jfdIqR7z0ACEowKqv2RlhCHVYotg",
+  authDomain: "shop-2b337.firebaseapp.com",
+  databaseURL: "https://shop-2b337.firebaseio.com",
+  projectId: "shop-2b337",
+  storageBucket: "shop-2b337.appspot.com",
+  messagingSenderId: "962003982333",
+  appId: "1:962003982333:web:a74318fde0750c0a692b94",
+  measurementId: "G-C5GMFKY66Y",
+};
+
+export default config;
