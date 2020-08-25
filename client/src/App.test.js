89725738636afc73a3c0012336d19f265102ebf3
bryan aviles <easyrun32@gmail.com>
2020-08-24 20:01:09 -0400

    hello

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import { App } from "./App";
+//commenting
 it("renders correctly", () => {
   const MockFunction = () => {};
 
