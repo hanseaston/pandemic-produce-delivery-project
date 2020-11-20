bb4d44ca7842086fc08dee22074870522cba8d97
Trim <trim.muharremi@studentet.uni-pr.edu>
2020-11-20 16:59:56 +0100

    Fixed Sign in page -align sign-in and sign-up at the top

--- client/src/components/sign-in/sign-in.jsx
+++ client/src/components/sign-in/sign-in.jsx
@@ -51,7 +51,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I already have an account</h2>
+        <h2 className="title">I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
