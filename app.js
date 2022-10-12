5a6a5f3cb456c2e71b85b5f6975c02c82fbc4af8
Hans Easton <hanreaston@gmail.com>
2022-10-12 10:29:05 -1000

    change port and update package.json

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 if (process.env.NODE_ENV !== "production") {
   const config = require("dotenv").config();
